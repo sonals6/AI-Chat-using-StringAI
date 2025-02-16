bd3fc8b688bfe50c43a0f0afa2247b1d99d889ad
unknown <you@example.com>
2025-02-16 14:55:29 +0530

    initial version

---
+++ src/main/java/com/proj_springAI/springDemo/ChatService.java
@@ -0,0 +1,15 @@
+import org.springframework.ai.chat.model.ChatModel;
+
+public class ChatService {
+    private final ChatModel chatModel;
+    
+    public ChatService(ChatModel chatModel)
+    {
+        this.chatModel = chatModel;
+    }
+
+    public String getResponse(String prompt)
+    {
+        return chatModel.call(prompt);
+    }
+}
---
+++ src/test/java/com/proj_springAI/springDemo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj_springAI.springDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
